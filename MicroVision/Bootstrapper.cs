9b60a08ff14027c83f3be8e0c3016903f0160352
yuanyi wu <wuyuanyi135@gmail.com>
2018-01-31 14:40:14 -0500

    Add project files.

---
+++ MicroVision/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Unity;
+using System.Windows;
+using MicroVision.Views;
+
+namespace MicroVision
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<MainWindow>();
+        }
+        protected override void InitializeShell()
+        {
+            if (Application.Current.MainWindow != null)
+                Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ MicroVision/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroVision.ViewModels
+{
+    
+    class MainWindowViewModel
+    {
+        public string Title { get; set; } = "MicroVision";
+    }
+}
