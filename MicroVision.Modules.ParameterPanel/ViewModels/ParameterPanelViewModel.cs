c3c604b010bf753e1e4a1653982140186bb0dd14
Yuanyi Wu <wuyuanyi135@gmail.com>
2018-02-01 03:52:01 -0500

    Test how to setproperty to notify the change of a customized class. will modify the parameter panel to data template-driven mode.

---
+++ MicroVision.Modules.ParameterPanel/ViewModels/ParameterPanelViewModel.cs
@@ -0,0 +1,37 @@
+﻿using MicroVision.Modules.ParameterPanel.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Collections.ObjectModel;
+
+namespace MicroVision.Modules.ParameterPanel.ViewModels
+{
+    public class ParameterPanelViewModel : BindableBase
+    {
+        //private ExposureTimeParameter exposureTimeParameter = new ExposureTimeParameter();
+        //public ExposureTimeParameter ExposureTime
+        //{
+        //    get => exposureTimeParameter;
+        //    set
+        //    {
+        //        SetProperty(ref exposureTimeParameter, value);
+        //    }
+        //}
+        public ExposureTimeParameter ExposureTime { get; } = new ExposureTimeParameter();
+        public DelegateCommand Command { get; }
+        public ParameterPanelViewModel()
+        {
+            Command = new DelegateCommand(Executed);
+        }
+
+        private void Executed()
+        {
+            ExposureTime.Value++;
+        }
+    }
+}
