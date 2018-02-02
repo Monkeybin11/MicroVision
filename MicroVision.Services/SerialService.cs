4e87a24b0402bfbc1694583d77e403e9bf41c260
Yuanyi Wu <wuyuanyi135@gmail.com>
2018-02-02 14:06:40 -0500

    move project model to a standalone project

---
+++ MicroVision.Services/SerialService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Events;
+
+namespace MicroVision.Services
+{
+    public class SerialService
+    {
+        private readonly IParameterServices _parameterServices;
+        private readonly IEventAggregator _eventAggregator;
+
+        public SerialService(IParameterServices parameterServices, IEventAggregator eventAggregator)
+        {
+            _parameterServices = parameterServices;
+            _eventAggregator = eventAggregator;
+            
+        }
+    }
+}
