12a3eb03da6aadf97cb08fe99b0cd2221c295ab7
Yuanyi Wu <wuyuanyi135@gmail.com>
2018-02-01 19:24:05 -0500

    Statuspanel: indicator done

--- MicroVision.Modules.StatusPanel/ViewModels/StatusPanelViewModel.cs
+++ MicroVision.Modules.StatusPanel/ViewModels/StatusPanelViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace MicroVision.Modules.StatusPanel.ViewModels
 {
@@ -17,9 +18,26 @@ namespace MicroVision.Modules.StatusPanel.ViewModels
             set { SetProperty(ref _message, value); }
         }
 
+        private Boolean _comConnected;
+        public Boolean ComConnected
+        {
+            get { return _comConnected; }
+            set { SetProperty(ref _comConnected, value); }
+        }
+
         public StatusPanelViewModel()
         {
             Message = "View A from your Prism Module";
+            Timer timer = new Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Enabled = true;
+            
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            ComConnected = !ComConnected;
         }
     }
 }
