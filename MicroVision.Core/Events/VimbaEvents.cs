227313be4bb1d109c4985ea3051e50edab9520e5
Yuanyi Wu <wuyuanyi135@gmail.com>
2018-02-05 00:55:15 -0500

    initialize vimba events

---
+++ MicroVision.Core/Events/VimbaEvents.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Events;
+
+namespace MicroVision.Core.Events
+{
+    #region Requests
+    public class VimbaListUpdateRequestedEvent : PubSubEvent { }
+    public class VimbaConnectionRequestedEvent : PubSubEvent { }
+    public class VimbaDisconnectionRequestedEvent : PubSubEvent { }
+    public class VimbaCaptureStartRequestedEvent : PubSubEvent { } 
+    public class VimbaCaptureStopRequestedEvent : PubSubEvent { }
+    #endregion
+
+    #region Emission
+    public class VimbaConnectedEvent : PubSubEvent { }
+    public class VimbaDisconnectedEvent : PubSubEvent { }
+    public class VimbaFrameCapturedEvent : PubSubEvent { }
+    public class VimbaFrameSavedEvent : PubSubEvent { }
+    public class VimbaErrorOccuredEvent : PubSubEvent<string> { }
+
+    #endregion
+}
