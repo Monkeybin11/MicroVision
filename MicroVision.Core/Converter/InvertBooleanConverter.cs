9afb40a9224339951ef0b7137ef681275f9f8f75
Yuanyi Wu <wuyuanyi135@gmail.com>
2018-03-08 18:54:49 -0500

    disable the dropdowns when connected.

---
+++ MicroVision.Core/Converter/InvertBooleanConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MicroVision.Core.Converter
+{
+    public class InvertBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? true : !(bool) value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+    }
+}
